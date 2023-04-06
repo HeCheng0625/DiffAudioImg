27a3121b4195c8c70091ae23bcbd7ce4bc142ece
HeCheng0625 <119010319@link.cuhk.edu.cn>
2023-04-06 03:21:12 +0000

    commit

---
+++ audio_to_img_v1_2.sh
@@ -0,0 +1,18 @@
+export MODEL_DIR="runwayml/stable-diffusion-v1-5"
+export OUTPUT_DIR="/blob/v-yuancwang/DiffAudioImg/AudioControlNet_v1_2"
+
+accelerate launch train_controlnet.py \
+ --pretrained_model_name_or_path=$MODEL_DIR \
+ --output_dir=$OUTPUT_DIR \
+ --resolution=512 \
+ --learning_rate=9.6e-5 \
+ --train_batch_size=6 \
+ --gradient_accumulation_steps=2 \
+ --max_train_steps=500000 \
+ --checkpointing_steps=5000 \
+ --image_column="image" \
+ --conditioning_image_column="conditioning_mel" \
+ --caption_column="text" \
+ --proportion_empty_prompts=0.8 \
+ --tracker_project_name="train_controlnet_v1_2" \
+ --resume_from_checkpoint="/blob/v-yuancwang/DiffAudioImg/AudioControlNet_v1_2/checkpoint-60000"
