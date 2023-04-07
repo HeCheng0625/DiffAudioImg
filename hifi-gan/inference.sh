982683577ea3243349cb7bd7d536c194d673b998
HeCheng0625 <119010319@link.cuhk.edu.cn>
2023-04-07 08:47:16 +0000

    commit

---
+++ hifi-gan/inference.sh
@@ -0,0 +1,4 @@
+python /home/v-yuancwang/hifi-gan/inference_e2e.py \
+--input_mels_dir="/blob/v-yuancwang/audio_editing_test/inpainting/100000/4.0/mel" \
+--output_dir="/blob/v-yuancwang/audio_editing_test/inpainting/100000/4.0/wav" \
+--checkpoint_file="/blob/v-yuancwang/hifigan_cp/g_00285000"
